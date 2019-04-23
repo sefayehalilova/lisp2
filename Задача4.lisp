6819ce617d8b54f0da8f6926762860465b5f59a1
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2019-04-23 21:52:49 +0300

    Create Задача4.lisp

---
+++ Задача4.lisp
@@ -0,0 +1,20 @@
+;Задача 4
+;Определите функциональный предикат (каждый пред список), который истинен в том и только в том случае, 
+;когда, являющейся функциональным аргументом предикат "пред" истинен для всех элементов списка "список".
+
+(defun every-true (pred lst)
+	(null(mapcan(lambda (x)
+       (if (funcall pred x) nil (list T))) lst)
+     )
+)
+
+
+;(print(every-true 'numberp '(1 2 3 4)))
+;T
+;(print(every-true 'numberp '(x 1 2 3 4)))
+;NIL
+
+;(print(every-true 'atom '(1 2 3 4)))
+;T
+;(print(every-true 'atom '(1 (2 3 4))))
+;NIL
