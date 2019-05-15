bac1bea689a98a2be05e149506a8b028cffd9252
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2019-05-15 21:58:23 +0300

    Create задача14.lisp

---
+++ задача14.lisp
@@ -0,0 +1,9 @@
+; Определите функцию, которая возвращает в качестве значения
+; форму своего определения (DEFUN).
+
+(defun function1 (lst)
+	 `(defun function1 (lst))
+ )
+
+;(print(function1 't))
+;(DEFUN FUNCTION1 (LST))
